926ac486811619f03af1be58158c26de5e6bd5c0
not-learning <kaa.smart@gmail.com>
2025-05-08 04:59:42 +0500

    Relative sizes - working, need adjustment

--- lmnts.go
+++ lmnts.go
@@ -6,9 +6,15 @@ import (
 	"slices"
 )
 
+const startW, startH float32 = 450, 1000
+var scrW, scrH int
+var ratW, ratH float32 = 1, 1
+
 // TODO relative sizes
 // TODO: think: 0 for 0 size, -1 for autosize
 type size struct{ w, h float32 }
+
+type sizeRel struct{ wr, hr float32 }
 
 func (el *Lmnt) Size() (w, h float32) {
 	return el.w, el.h
@@ -16,6 +22,7 @@ func (el *Lmnt) Size() (w, h float32) {
 
 func (el *Lmnt) SetSize(w, h float32) {
 	el.w, el.h = w, h
+	//el.wr, el.hr = w * ratW, h * ratH
 }
 
 type point struct{ x, y float32 }
@@ -53,6 +60,7 @@ func (el *Lmnt) SetRow() { el.row = true }
 type Lmnt struct {
 	Name string
 	*size
+	*sizeRel
 	*rect
 	*layout
 }
@@ -60,9 +68,18 @@ type Lmnt struct {
 func New() *Lmnt {
 	return &Lmnt{
 		size:   &size{},
+		sizeRel:   &sizeRel{},
 		rect:   &rect{&point{}, &point{}},
 		layout: newLayout(),
 	}
+}
+
+func (el *Lmnt) Update(w, h int) {
+	if scrW == w && scrH == h { return }
+	scrW, scrH = w, h
+	ratW = float32(scrW) / startW
+	ratH = float32(scrH) / startH
+	el.DoAll()
 }
 
 func (el *Lmnt) Add(lls ...*Lmnt) {
@@ -90,7 +107,6 @@ func (el *Lmnt) IsAdded(ll *Lmnt) bool {
 	return false
 }
 
-// todo: check with kids as non-pointer
 func (el *Lmnt) Del(ll *Lmnt) {
 	el.kids = slices.DeleteFunc(el.kids, func(l *Lmnt) bool {
 		return ll == l
@@ -119,10 +135,16 @@ func (el *Lmnt) WalkUp(fn func(*Lmnt)) {
 }
 
 // ### The Mechanism
-func setTotals(el *Lmnt) {
+func setRelSize(el *Lmnt) {
+	el.wr = el.w * ratW
+	el.hr = el.h * ratW
+}
+
+func setTotalSizes(el *Lmnt) {
 	el.total.w, el.total.h = 0, 0
 	for _, ll := range el.kids {
-		mw, mh := myMax(ll.w, ll.total.w), myMax(ll.h, ll.total.h)
+		mw := myMax(ll.wr, ll.total.w)
+		mh := myMax(ll.hr, ll.total.h)
 		if el.row {
 			el.total.w += mw
 			el.total.h = myMax(mh, el.total.h)
@@ -137,20 +159,17 @@ func sizesList(fs, n float32, sl, st []float32) []float32 {
 	delta := fs / n
 	for {
 		done := true
+		if done { break }
+
 		for i := range st {
 			if st[i] > delta {
 				sl[i] = st[i]
 				fs -= st[i]
 				st[i] = 0
-				if n > 1 {
-					n--
-				} // todo: is this really necessary?
+				if n > 1 { n-- } // todo: is this really necessary?
 				delta = fs / n
 				done = false
 			}
-		}
-		if done {
-			break
 		}
 	}
 	for i := range sl {
@@ -161,18 +180,16 @@ func sizesList(fs, n float32, sl, st []float32) []float32 {
 	return sl
 }
 
-func (el *Lmnt) setClm() {
+func (el *Lmnt) setClmRect() {
 	fs := myMax(el.p2.y-el.p1.y, el.total.h)
 	n := float32(len(el.kids))
 	sl := make([]float32, len(el.kids))
 	st := make([]float32, len(el.kids))
 	for i, v := range el.kids {
-		if v.h > 0 {
-			sl[i] = v.h
-			fs -= v.h
-			if n > 1 {
-				n--
-			} // todo: is this really necessary?
+		if v.hr > 0 {
+			sl[i] = v.hr
+			fs -= v.hr
+			if n > 1 { n-- } // todo: is this really necessary?
 		} else {
 			st[i] = v.total.h
 		}
@@ -183,25 +200,23 @@ func (el *Lmnt) setClm() {
 	dy := el.p1.y
 	for i, v := range el.kids {
 		v.p1.x = el.p1.x
-		v.p2.x = v.p1.x + cmp.Or(v.w, w)
+		v.p2.x = v.p1.x + cmp.Or(v.wr, w)
 		v.p1.y = dy
 		dy += hl[i]
 		v.p2.y = dy
 	}
 }
 
-func (el *Lmnt) setRow() {
+func (el *Lmnt) setRowRect() {
 	fs := myMax(el.p2.x-el.p1.x, el.total.w)
 	n := float32(len(el.kids))
 	sl := make([]float32, len(el.kids))
 	st := make([]float32, len(el.kids))
 	for i, v := range el.kids {
-		if v.w > 0 {
-			sl[i] = v.w
-			fs -= v.w
-			if n > 1 {
-				n--
-			} // todo: is this really necessary?
+		if v.wr > 0 {
+			sl[i] = v.wr
+			fs -= v.wr
+			if n > 1 { n-- } // todo: is this really necessary?
 		} else {
 			st[i] = v.total.w
 		}
@@ -213,7 +228,7 @@ func (el *Lmnt) setRow() {
 
 	for i, v := range el.kids {
 		v.p1.y = el.p1.y
-		v.p2.y = v.p1.y + cmp.Or(v.h, h)
+		v.p2.y = v.p1.y + cmp.Or(v.hr, h)
 
 		v.p1.x = dx
 		dx += wl[i]
@@ -226,13 +241,14 @@ func setRects(el *Lmnt) {
 		return
 	}
 	if el.row {
-		el.setRow()
+		el.setRowRect()
 	} else {
-		el.setClm()
+		el.setClmRect()
 	}
 }
 
 func (el *Lmnt) DoAll() {
-	el.WalkUp(setTotals)
+	el.WalkDown(setRelSize)
+	el.WalkUp(setTotalSizes)
 	el.WalkDown(setRects)
 }
